e8f6fb40ebbe56da4a1046820e6c95f4f6cc45ff
jayzizzle <dj.jonzamora@gmail.com>
2021-09-03 17:28:22 -0700

    add toggle for left player

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,42 +1,50 @@
 const artists = [
     {
         id: 0,
-        alias: '2Pac',
+        stageName: '2Pac',
+        alias: 'Makaveli',
         fullName: 'Tupac Shakur'
     },
     {
         id: 1,
-        alias: 'BIG',
+        stageName: 'BIG',
+        alias: 'Big Poppa',
         fullName: 'Christopher Wallace'
     },
     {
         id: 2,
-        alias: 'Drake',
+        stageName: 'Drake',
+        alias: 'Champagne Papi',
         fullName: 'Aubrey Graham'
     },
     {
         id: 3,
-        alias: 'Eminem',
+        stageName: 'Eminem',
+        alias: 'Slim Shady',
         fullName: 'Marshall Mathers'
     },
     {
         id: 4,
-        alias: 'Jay-Z',
+        stageName: 'Jay-Z',
+        alias: 'Young Hov',
         fullName: 'Shawn Carter'
     },
     {
         id: 5,
-        alias: 'Kanye',
+        stageName: 'Kanye',
+        alias: 'Yeezy',
         fullName: 'Kanye West'
     },
     {
         id: 6,
-        alias: 'Lil Wayne',
+        stageName: 'Lil Wayne',
+        alias: 'Weezy F. Baby',
         fullName: 'Dwayne Carter'
     },
     {
         id: 7,
-        alias: 'Nas',
+        stageName: 'Nas',
+        alias: 'Nas Escobar',
         fullName: 'Nasir Jones'
     }
 ]
--- src/scripts/player_select.js
+++ src/scripts/player_select.js
@@ -11,22 +11,58 @@ class PlayerSelect {
             box.setAttribute('data-id', i);
             ul.append(box);
         }
+
+        this.defaultLoad();
+        ul.addEventListener('click', this.handleClick.bind(this));
         ul.addEventListener('mouseover', this.handleHover.bind(this));
+        ul.addEventListener('mouseout', this.handleOffHover.bind(this));
     }
     defaultLoad() {
-        
+        const displayLeftName = document.getElementById('left-player-name');
+        const displayLeftAlias = document.getElementById('left-player-alias');
+        const displayRightName = document.getElementById('right-player-name');
+        const displayRightAlias = document.getElementById('right-player-alias');
+        displayLeftName.innerHTML = this.leftSelection.stageName;
+        displayLeftAlias.innerHTML = this.leftSelection.alias;
+        displayRightName.innerHTML = this.rightSelection.stageName;
+        displayRightAlias.innerHTML = this.rightSelection.alias;
+
+        const list = document.getElementsByClassName('li-player-select');
+        list[this.leftSelection.id].classList.add('left-selection');
+        list[this.rightSelection.id].classList.add('right-selection');
+    }
+
+    handleClick(e) {
+        e.stopPropagation();
+        let ele = e.target;
+
+        const oldLeftSelection = document.getElementsByClassName('left-selection');
+        oldLeftSelection[0].classList.remove("left-selection");
+
+        ele.classList.add('left-selection');
+        const artist = this.artists[ele.dataset.id];
+        this.leftSelection = artist;
+        this.hoverChain(ele);
     }
 
     handleHover(e) {
-        let ele = e.target;
         e.stopPropagation();
+        let ele = e.target;
         this.hoverChain(ele);
+    }
+
+    handleOffHover(e) {
+        e.stopPropagation();
+        let ele = e.target;
+        this.defaultLoad();
     }
 
     hoverChain(ele) {
         const artist = this.artists[ele.dataset.id];
         const displayName = document.getElementById('left-player-name');
-        if (artist) displayName.innerHTML = artist.alias;
+        const displayAlias = document.getElementById('left-player-alias');
+        if (artist) displayName.innerHTML = artist.stageName;
+        if (artist) displayAlias.innerHTML = artist.alias;
     }
 }
 
